1d527133da1a8d06bd3a127ed31f3bbedc373f40
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-11 14:35:13 +0600

    added client site credientcial

--- client/src/firebase.init.js
+++ client/src/firebase.init.js
@@ -1,19 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
+import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCk7iTgiGpzXvhjitNRf3Oa1EqkKZqBzNo",
-  authDomain: "ema-john-simple-85c01.firebaseapp.com",
-  projectId: "ema-john-simple-85c01",
-  storageBucket: "ema-john-simple-85c01.appspot.com",
-  messagingSenderId: "598631523012",
-  appId: "1:598631523012:web:16797e70a7e5e9f0acdf91"
+  apiKey: "AIzaSyCp3h0g50Xiv0avivp62LoBTIAXkaqs8Oc",
+  authDomain: "auth-template-firebase.firebaseapp.com",
+  projectId: "auth-template-firebase",
+  storageBucket: "auth-template-firebase.appspot.com",
+  messagingSenderId: "151434870168",
+  appId: "1:151434870168:web:b718d93d5b27f17ded7fed",
+  measurementId: "G-7SWV1Z2V0H"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
